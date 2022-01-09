da8e6eef81f0a5bbba198d470bd5e9f9657d5a32
Shantanu Maskeri <shaan.mask@gmail.com>
2022-01-09 16:18:39 +0400

    Fire Safety AR
    An interactive and educational experience  on the fire extinguisher safety protocol - P.A.S.S

---
+++ Assets/Scripts/ApplicationLifeCycle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplicationLifeCycle : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void ReloadScene()
+	{
+        SceneManager.LoadScene(0);
+	}
+
+}
